350ec4f704d7035685447b97acc1ea965699e694
Khushaal Choithramani <khushaal.choithramani@gmail.com>
2024-07-26 14:56:28 +0530

    twitter meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@khush.dev" />
+        <meta
+          name="twitter:title"
+          content="Simple JS Code - A Curated Directory of JS Code"
+        />
+        <meta
+          name="twitter:description"
+          content="A hand-picked collection of more than 150+ JS code snippets that's constantly updated, and curated for you to include it in your next project."
+        />
+        <meta
+          name="twitter:image"
+          content="https://i.ibb.co/g7JTQQm/Screenshot-2024-07-26-at-14-51-54.png"
+        />
+      </Head>
+      <meta
+        name="twitter:image"
+        content="https://styles/assetsOptimized/ourStory/StoreFront.jpeg"
+      ></meta>
+
       <body className={inter.className}>
         <Toaster />
         <Navbar />
